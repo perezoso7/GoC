4370c7ecd9201e7e22e37f080eae6204a649c682
Mginx <max@ginter-wersant.lu>
2017-03-11 17:16:04 +0100

    import data from github

--- calculation/importCalc.js
+++ calculation/importCalc.js
@@ -1,20 +1,3 @@
-function getJSON(url, success) {
-
-    var ud = '_' + +new Date,
-        script = document.createElement('script'),
-        head = document.getElementsByTagName('head')[0]
-            || document.documentElement;
-
-    window[ud] = function (data) {
-        head.removeChild(script);
-        success && success(data);
-    };
-
-    script.src = url.replace('callback=?', 'callback=' + ud);
-    head.appendChild(script);
-
-}
-
-getJSON('https://raw.githubusercontent.com/ginma89/GoC/master/data/testData.json', function (data) {
+$.getJSON("https://raw.githubusercontent.com/ginma89/GoC/master/data/testData.json", function (data) {
     console.log(data);
 });
